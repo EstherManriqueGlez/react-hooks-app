e85be882e68c01a22f66a6f9c92f2ae376fc9152
Esther Manrique <esthermanriqueepam@gmail.com>
2026-01-22 11:54:26 -0600

    feat: enhance AboutPage and LoginPage with navigation links.

--- src/09-useContext/pages/auth/LoginPage.tsx
+++ src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,6 +1,22 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Link } from 'react-router';
 
 export const LoginPage = () => {
   return (
-    <div>Login Page</div>
-  )
-}
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Login Page</h1>
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="number" placeholder="User ID" />
+
+        <Button type="submit">Login</Button>
+      </form>
+
+      <Link to="/about" className="hover:text-blue-500 underline text-2xl">
+        <Button variant="ghost">Go to About Page</Button>
+      </Link>
+    </div>
+  );
+};
--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,6 +1,14 @@
+import { Button } from '@/components/ui/button';
 
 export const ProfilePage = () => {
   return (
-    <div>Profile Page</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl">User Profile</h1>
+      <hr className="my-4" />
+
+      <pre className="my-4">{JSON.stringify({}, null, 2)}</pre>
+
+      <Button variant="destructive">Logout</Button>
+    </div>
+  );
+};
